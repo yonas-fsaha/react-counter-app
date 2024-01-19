aa5c18095b0af0df09ad9a0fc95c90c50cbe80f0
Yonas Fsaha <umyonas7@gmail.com>
2024-01-19 14:25:17 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer } from 'react';
+import React from 'react';
+
+
+const ACTION = {
+    INCRIMENT: "incriment",
+
+    DECRIMENT: "decriment"
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'incriment':
+            return { count: state.count + 1 };
+        case 'decriment':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+
+
+
+    return ( <
+        div className = "app" >
+        <
+        h2   style = {
+            { fontSize:"40px" }
+        }> This is counter app < /h2> <
+        p > click the pluse button to add and the minus button to substruct < /p>
+
+        <
+        div className = 'mainpart' >
+        <
+        p className='resultp'> count: { state.count } < /p>
+        <div className='butons'>
+        <
+        button className = "addbutton"
+        style = {
+            { marginRight: "20px" }
+        }
+        onClick = {
+            () => {
+                dispatch({ type: ACTION.INCRIMENT })
+            }
+        } > + < /button> 
+
+        <
+        button className = "substractbutton"
+        onClick = {
+            () => {
+                dispatch({ type: ACTION.DECRIMENT })
+            }
+        } > - < /button>
+        </div>
+
+        <
+        /div>
+
+        <
+        /div >
+
+    );
 }
+
 
 export default App;
